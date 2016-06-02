18c0e04ac43c44425a35837def30e7ee3a77bbde
Rebeca <daw15-09@c16x09.iescierva.net>
2016-06-02 16:38:13 +0200

    Implementación clase correo test.

--- JV2015-GruposW/src/modelo/test/CorreoTest.java
+++ JV2015-GruposW/src/modelo/test/CorreoTest.java
@@ -4,11 +4,46 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import modelo.Correo;
+
+/**
+ * Realiza un test sobre la clase Correo del modelo.
+ * @author Rebeca Fernández
+ * 26/05/2016
+ */
+
 public class CorreoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCorreoString() {
+	
+		Correo correo = new Correo("correo@correo");
+		assertTrue(correo != null);
+	
+		}
+	
+	@Test
+	public void testCorreoCorreo() {
+		
+		Correo correo = new Correo("correo@correo");
+		assertTrue(correo != null);
+			
+	}
+	
+	@Test
+	public void testSetTexto() {
+		Correo correo = new Correo("correo@correo");
+		assertNotNull(correo); 
+		
+		
 	}
 
+
+	@Test
+	public void testGetTexto() {
+		Correo correo = new Correo("correo@correo");
+		assertNotNull(correo.getTexto());
+		
+		
+	}
 }
